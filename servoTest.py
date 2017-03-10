e9c7e8e5ab9dede05ca46426873886a7c39f9f28
Raúl Hernández <raul@lafuentelab.org>
2017-03-10 14:49:16 -0600

    Update and rename servo.py to servoTest.py

---
+++ servoTest.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 100)
+pwm.start(0)
+time.sleep(0.5)
+pwm.stop()
+
+def movServo(angle):
+    duty = float(angle)/10.0 + 2.5
+    #pwm.ChangeDutyCycle(duty)
+    pwm.start(duty)
+    time.sleep(0.5)
+    pwm.stop()
